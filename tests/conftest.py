00b14b375f6a3cc2db186aceee72e6d33341b790
Marc Portier <marc.portier@gmail.com>
2025-02-24 21:20:47 +0100

    refactored in preparation of next test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
+from contextlib import contextmanager
 from importlib.machinery import SourceFileLoader
 from importlib.util import module_from_spec, spec_from_file_location
 from logging import Logger, getLogger
+import os
+from pathlib import Path
+import tempfile
+
+from dotenv import load_dotenv
 
 log: Logger = getLogger(__name__)
 
@@ -17,3 +23,25 @@ def load_source(modname, filename):
 
 
 entrypoint = load_source("entrypoint", "entrypoint.py")
+root = Path(entrypoint.__file__).parent
+
+
+@contextmanager
+def tempdir_path():
+    load_dotenv()
+    tmpdir = os.getenv("TEST_OUTFOLDER")
+    if tmpdir:
+        tmpdir = Path(tmpdir)
+        tmpdir.mkdir(exist_ok=True, parents=True)
+        log.info(
+            f"Configured outfolder {tmpdir=} "
+            "allows the output to be manually verified."
+        )
+        yield tmpdir
+    else:
+        log.info(
+            "Temporary outfolder removed at end of test. "
+            "Use environment TEST_OUTFOLDER to specify a permanent one."
+        )
+        with tempfile.TemporaryDirectory() as tmpdir:
+            yield Path(tmpdir)
--- tests/test_entrypoint.py
+++ tests/test_entrypoint.py
@@ -1,53 +1,36 @@
 import filecmp
-import os
-import tempfile
 from pathlib import Path
-
-from conftest import entrypoint, log
-from dotenv import load_dotenv
+from conftest import entrypoint, root, tempdir_path
 from rdflib import Graph
 from sema.commons.glob import getMatchingGlobPaths
 
 
-def test_cli():
-    assert entrypoint._main
-    root = Path(entrypoint.__file__).parent
+def verify_build(outdir: Path) -> None:
+    # verify results
+    resultfiles = getMatchingGlobPaths(
+        outdir, includes=["*.ttl"], makeRelative=False
+    )
+    assert len(resultfiles) == 2
+    # compare the ttl files, should be exactly the same
+    assert filecmp.cmp(resultfiles[0], resultfiles[1], shallow=True)
+    # try parsing the ttl files too
+    for rf in resultfiles:
+        assert rf.exists()
+        assert rf.stat().st_size > 0
+        Graph().parse(str(rf), format="ttl")
 
-    def build_and_verify(outdir: str | Path) -> None:
-        outdir = Path(outdir)  # ensure Path type
+
+def test_basics():
+    assert entrypoint._main
+
+    def build_and_verify(outdir: Path) -> None:
         entrypoint._main(
             workfile=root / "tests/test-work.yml",
             rocrateroot=root / "tests/data",
             templateroot=root / "tests/templates",
             resultsroot=outdir,
         )
-        # verify results
-        resultfiles = getMatchingGlobPaths(
-            outdir, includes=["*.ttl"], makeRelative=False
-        )
-        assert len(resultfiles) == 2
-        # compare the ttl files, should be exactly the same
-        assert filecmp.cmp(resultfiles[0], resultfiles[1], shallow=True)
-        # try parsing the ttl files too
-        for rf in resultfiles:
-            assert rf.exists()
-            assert rf.stat().st_size > 0
-            Graph().parse(str(rf), format="ttl")
+        verify_build(outdir)
 
-    load_dotenv()
-    tmpdir = os.getenv("TEST_OUTFOLDER")
-    if tmpdir:
-        tmpdir = Path(tmpdir)
-        tmpdir.mkdir(exist_ok=True, parents=True)
-        log.info(
-            f"Configured outfolder {tmpdir=} "
-            "allows the output to be manually verified."
-        )
+    with tempdir_path() as tmpdir:
         build_and_verify(tmpdir)
-    else:
-        log.info(
-            "Temporary outfolder removed at end of test. "
-            "Use environment TEST_OUTFOLDER to specify a permanent one."
-        )
-        with tempfile.TemporaryDirectory() as tmpdir:
-            build_and_verify(tmpdir)
